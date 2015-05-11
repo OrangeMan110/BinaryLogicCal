7e4f416be602a7a1dcf621bc7aa7e60ce6f97d2a
hujewelz <hujewelz@163.com>
2015-05-11 22:17:08 +0800

    界面搭建

--- BinaryLogicCal/ViewController.m
+++ BinaryLogicCal/ViewController.m
@@ -7,8 +7,16 @@
 //
 
 #import "ViewController.h"
+#import "CalculatorBrain.h"
 
-@interface ViewController ()
+@interface ViewController () {
+    NSMutableArray *_input;
+    CalculatorBrain *_calBrain;
+    NSString *_binarytitle;
+}
+@property (weak, nonatomic) IBOutlet UILabel *decimalLab;
+
+@property (weak, nonatomic) IBOutlet UILabel *binaryLab;
 
 @end
 
@@ -16,12 +24,41 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    _input = [NSMutableArray array];
+    
+    _calBrain = [[CalculatorBrain alloc] init];
+    
+    
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
+- (IBAction)numberInput:(UIButton *)sender {
+    if ([sender.titleLabel.text isEqualToString:@"C"]) {
+        self.decimalLab.text = @"0";
+        self.binaryLab.text = @"0";
+        [_input removeAllObjects];
+        [_calBrain clear];
+        return;
+    }
+    [_input addObject:sender.titleLabel.text];
+    if (_input.count > 18) return;
+    
+    NSString *title = @"";
+    for (NSString *str in _input) {
+        title = [title stringByAppendingString:str];
+    }
+   
+    self.decimalLab.text = title;
+    _binarytitle = title;
+     //NSLog(@"_binarytitle :%@", _binarytitle);
+    
 }
 
+- (IBAction)operationAction:(UIButton *)sender {
+    [_input removeAllObjects];
+    self.binaryLab.text  = [_calBrain number:_binarytitle operation:sender.titleLabel.text];
+    
+   // [_calBrain calculateResult];
+    
+}
 @end
