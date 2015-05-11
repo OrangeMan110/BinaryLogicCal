0aa240adbff10fe4957e8c4ef050c9f777ef762f
hujewelz <hujewelz@163.com>
2015-05-11 16:55:13 +0800

    二进制计算器

---
+++ BinaryLogicCal/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BinaryLogicCal
+//
+//  Created by jewelz on 15/5/11.
+//  Copyright (c) 2015年 yangtzeu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BinaryLogicCal/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BinaryLogicCal
+//
+//  Created by jewelz on 15/5/11.
+//  Copyright (c) 2015年 yangtzeu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
